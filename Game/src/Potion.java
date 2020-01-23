f2236eeafba059fdf40da3bc1bb1bc896b888751
Kang <kang4722@cs.washington.edu>
2020-01-23 08:32:31 -0800

    Original

--- Game/src/Potion.java
+++ Game/src/Potion.java
@@ -3,44 +3,20 @@
 public class Potion {
 
 
-
-
-
-
-
     public String getName() {
         return name;
     }
-
-
-
     public void setName(String name) {
         this.name = name;
     }
-
-
-
     private int price;
     private String name;
-
-
     Potion(String name){
-
-
         this.name = name;
-
-
     }
-
-
     public String toString() {
         return name;
     }
-
-
-
-
-
 
 }
 
